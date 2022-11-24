6c7cdbc2fabf7f4edfaad344e8e0eb42fe9f22de
moriya <moriya.hadad@campus.technion.ac.il>
2022-11-24 20:18:27 +0200

    remove function finished

--- main.cpp
+++ main.cpp
@@ -2,20 +2,20 @@
 
 
 template<class T>
-struct Node
-{
-    Node<T>* father;
-    Node<T>* son_smaller;
-    Node<T>* son_larger;
-    T data;
-    int height;
-};
-
-
-template<class T>
 class AVL_Tree
  {
-    Node<T>*  root;
+
+    struct Node
+    {
+        Node* father;
+        Node* son_smaller;
+        Node* son_larger;
+        T data;
+        int height;
+    };
+
+    Node*  root;
+
 public:
     AVL_Tree()
     {
@@ -28,7 +28,7 @@ public:
 
     ~AVL_Tree(); //do after
 
-    int height(Node<T> *t)
+    int height(Node *t)
     {
         if (t == nullptr)
             return -1;
@@ -39,16 +39,16 @@ public:
         }
     }
 
-    int bf(Node<T> *t)
+    int bf(Node *t)
     {
         return t->son_smaller->height - t->son_larger->height;
     }
 
-    Node<T>* search(const T& data)
+    Node* search(const int data)
     {
         if (root == nullptr)
             return nullptr;
-        Node<T>* t= root;
+        Node* t= root;
         while(t != nullptr)
         {
             if(t->data == data)
@@ -67,40 +67,37 @@ public:
         return nullptr;
     }
 
-     bool isLeaf (const Node<T>* node)
-     {
-         if (node->son_larger == nullptr && node->son_smaller == nullptr)
-             return true;
-         return false;
-     }
-
-    Node<T>* rotate_LL(Node<T>* t) {
-        Node<T> *tmp1 = t;
-        Node<T> *tmp2 = t->son_smaller;
-        tmp1->son_smaller = tmp2->son_larger;
-        tmp1->son_smaller->father = tmp1;
-        tmp2->son_larger = tmp1;
-        tmp2->father = tmp1->father;
-        tmp1->father = tmp2;
-        return tmp2;
+    Node* rotate_LL(Node* t) {
+        Node *temp1 = t;
+        Node *temp2 = t->son_smaller;
+        temp1->son_smaller = temp2->son_larger;
+        temp1->son_smaller->father = temp1;
+        temp2->son_larger = temp1;
+        temp2->father = temp1->father;
+        temp1->father = temp2;
+        temp2->height = height(temp2);
+        temp1->height = height(temp1);
+        return temp2;
     }
 
-    Node<T>* rotate_RR(Node<T>* t) {
-        Node<T> *temp1 = t;
-        Node<T> *temp2 = t->son_larger;
+    Node* rotate_RR(Node* t) {
+        Node *temp1 = t;
+        Node *temp2 = t->son_larger;
         temp1->son_larger = temp2->son_smaller;
         temp1->son_larger->father = temp1;
         temp2->son_smaller = temp1;
         temp2->father = temp1->father;
         temp1->father = temp2;
+        temp2->height = height(temp2);
+        temp1->height = height(temp1);
         return temp2;
     }
 
-    Node<T>* rotate_RL(Node<T>* t)
+    Node* rotate_RL(Node* t)
     {
-        Node<T> *temp1 = t;  //points to A
-        Node<T> *temp2 = t->son_larger;  //points to B
-        Node<T> *temp3 = t->son_larger->son_smaller;  //points to C
+        Node *temp1 = t;  //points to A
+        Node *temp2 = t->son_larger;  //points to B
+        Node *temp3 = t->son_larger->son_smaller;  //points to C
         temp1->son_larger = temp3->son_smaller;  //right side of A point to left of C
         temp3->son_smaller->father = temp1;  //right side of C points to new father A
         temp2->son_smaller = temp3->son_larger;  //left side of B points to right side of C
@@ -110,14 +107,17 @@ public:
         temp1->father = temp3;  //Apoints to father C
         temp3->father = t->father;  //C points to A's father (C's father pointer)
         temp2->father = temp3;  //B points to father C
+        temp1->height = height(temp1);
+        temp2->height = height(temp2);
+        temp3->height = height(temp3);
         return temp3;  // return new C to be t.
     }
 
-    Node<T>* rotate_LR(Node<T>* t)
+    Node* rotate_LR(Node* t)
     {
-        Node<T> *temp1 = t;
-        Node<T> *temp2 = t->son_smaller;
-        Node<T> *temp3 = t->son_smaller->son_larger;
+        Node *temp1 = t;
+        Node *temp2 = t->son_smaller;
+        Node *temp3 = t->son_smaller->son_larger;
         temp1->son_smaller = temp3->son_larger;
         temp3->son_larger->father = temp1;
         temp2->son_larger = temp3->son_smaller;
@@ -127,16 +127,19 @@ public:
         temp1->father = temp3;
         temp3->father = t->father;
         temp2->father = temp3;
+        temp1->height = height(temp1);
+        temp2->height = height(temp2);
+        temp3->height = height(temp3);
         return temp3;
     }
 
-    Node<T>* insert(Node<T>* t,const T& data)
+    Node* insert(Node* t,const T& data)
     {
         if (t == nullptr)
         {
             try
             {
-                Node<T> *base = &new(Node<T>);
+                Node *base = &new(Node);
                 base->data = data;
                 base->son_larger = nullptr;
                 base->son_smaller = nullptr;
@@ -152,93 +155,131 @@ public:
             if (t->data < data)
             {
                 t->son_larger = insert(t->son_larger, data);
-                if(t->son_larger->data == data)
-                    t->son_larger->father = t;
+                t->son_larger->father = t;
             }
             else if (t->data > data)
             {
                 t->son_smaller = insert(t->son_smaller, data);
-                if(t->son_smaller->data == data)
-                    t->son_smaller->father = t;
+                t->son_smaller->father = t;
             }
             else
             {
                 return nullptr;
             }
             t->height = height(t);
-            if (balancefactor(t) == 2 && balancefactor(t->son_smaller) >= 0)
-            {
-                t = rotate_LL(t);
-            }
-            if (balancefactor(t) == 2 && balancefactor(t->son_smaller) < 0) {
-                t = rotate_LR(t);
-            }
-            if (balancefactor(t) == -2 && balancefactor(t->son_larger) <= 0) {
-                t = rotate_RR(t);
-            }
-            if (balancefactor(t) == -2 && balancefactor(t->son_larger) == 1) {
-                t = rotate_RL(t);
-            }
+            t = fix_balance(t);
             return t;
         }
     }
 
+    Node* fix_balance (const Node* t)
+    {
+        if (bf(t) == 2 && bf(t->son_smaller) >= 0)
+        {
+            t = rotate_LL(t);
+        }
+        if (bf(t) == 2 && bf(t->son_smaller) < 0) {
+            t = rotate_LR(t);
+        }
+        if (bf(t) == -2 && bf(t->son_larger) <= 0) {
+            t = rotate_RR(t);
+        }
+        if (bf(t) == -2 && bf(t->son_larger) == 1) {
+            t = rotate_RL(t);
+        }
+        return t;
+    }
+
     bool remove (int num)
     {
-        if (root == nullptr)
+        Node *ptr = search(num);
+        if (ptr == nullptr)
             return false;
-        Node<T> *ptr = root;
-        while (ptr != nullptr)
+        Node* ptr_father = ptr->father;
+        if (isLeaf(ptr))
         {
-            if (ptr->data > num)
-                ptr = ptr->son_smaller;
-            else if (ptr->data < num)
-                ptr = ptr->son_larger;
+            remove_leaf(ptr);
+        }
+        else if (ptr->son_larger == nullptr || ptr->son_smaller == nullptr)
+            remove_half_leaf(ptr);
+        else
+        {
+            Node* temp1 = ptr->son_larger;
+            Node* temp2 = temp1->son_smaller;
+            while (temp2->son_smaller != nullptr)
+                temp2 = temp2->son_smaller;
+            ptr->son_larger = temp2->son_larger;
+            if (temp2->son_larger != nullptr)
+                temp2->son_larger->father = ptr;
+            temp2->son_larger = temp1;
+            temp1->father = temp2;
+            temp1 = temp2->father;
+            temp2->father = ptr->father;
+            if (ptr->father != nullptr && ptr->father->data > ptr->data)
+                ptr->father->son_smaller = temp2;
+            else if (ptr->father != nullptr)
+                ptr->father->son_larger = temp2;
+            ptr->father = temp1;
+            temp1->son_smaller = ptr;
+            if (isLeaf(ptr))
+                remove_leaf(ptr);
             else
+                remove_half_leaf(ptr);
+            while (temp2->father != nullptr)
             {
-                if (isLeaf(ptr))
-                {
-                    if (ptr->father->data > ptr->data)
-                        ptr->father->son_smaller = nullptr;
-                    else
-                        ptr->father->son_larger = nullptr;
-                    return true;
-                }
-                else if (ptr->son_larger == nullptr)
-                {
-                    if (ptr->father->data > ptr->data)
-                        ptr->father->son_smaller = ptr->son_smaller;
-                    else
-                        ptr->father->son_larger = ptr->son_smaller;
-                    ptr->son_smaller->father = ptr->father;
-                }
-                else if (ptr->son_smaller == nullptr)
-                {
-                    if (ptr->father->data > ptr->data)
-                        ptr->father->son_smaller = ptr->son_larger;
-                    else
-                        ptr->father->son_larger = ptr->son_larger;
-                    ptr->son_larger->father = ptr->father;
-                }
-                else
-                {
-                    Node<T>* temp = ptr->son_larger;
-                    while (temp->son_smaller != nullptr)
-                        temp = temp->son_smaller;
-                    temp->father->son_smaller = ptr;
-                    if (ptr->father->data > ptr->data)
-                        ptr->father->son_smaller = temp;
-                    else
-                        ptr->father->son_larger = temp;
-
-                    //father = null?
-
-                    //balance height
-
-                }
-
+                temp2->height = height(temp2);
+                temp2 = fix_balance(temp2);
+                temp2 = temp2->father;
             }
         }
+        fix_height(ptr_father);
+        return true;
     }
+
+    bool isLeaf (const Node* node)
+    {
+        if (node->son_larger == nullptr && node->son_smaller == nullptr)
+            return true;
+        return false;
+    }
+
+    void remove_leaf (const Node* ptr)
+    {
+        if (ptr->father->data > ptr->data)
+            ptr->father->son_smaller = nullptr;
+        else
+            ptr->father->son_larger = nullptr;
+    }
+
+    void remove_half_leaf (const Node* ptr)
+    {
+        if (ptr->son_larger == nullptr)
+        {
+            if (ptr->father != nullptr && ptr->father->data > ptr->data)
+                ptr->father->son_smaller = ptr->son_smaller;
+            else if (ptr->father != nullptr)
+                ptr->father->son_larger = ptr->son_smaller;
+            ptr->son_smaller->father = ptr->father;
+        }
+        else
+        {
+            if (ptr->father != nullptr && ptr->father->data > ptr->data)
+                ptr->father->son_smaller = ptr->son_larger;
+            else  if (ptr->father != nullptr)
+                ptr->father->son_larger = ptr->son_larger;
+            ptr->son_larger->father = ptr->father;
+        }
+    }
+
+    void fix_height (const Node* node)
+    {
+        while (node != nullptr && node->height != height(node))
+        {
+            node->height = height(node);
+            node = fix_balance(node);
+            node = node->father;
+        }
+    }
+
 
 };
