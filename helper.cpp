2f2c1f5794bc7b1dfb128505f5138280c861c7df
moriyahadad <moriya.hadad97@gmail.com>
2022-11-30 19:09:48 +0200

    helper

---
+++ helper.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "AVL_tree.h"
+
+using namespace std;
+
+
+int main()
+{
+
+
+
+    AVL_Tree<int, intBigger> tree;
+
+    tree.insert_to_tree(6);
+    tree.insert_to_tree(5);
+    tree.insert_to_tree(8);
+    tree.insert_to_tree(4);
+    tree.insert_to_tree(4);
+    tree.insert_to_tree(3);
+    tree.insert_to_tree(2);
+    tree.remove(3);
+    tree.insert_to_tree(1);
+    int* to_print = new int[6];
+    tree.print_tree(to_print);
+    for (int i=0; i<6; i++) {
+        cout << to_print[i];
+    }
+    return 0;
+
+
+}
